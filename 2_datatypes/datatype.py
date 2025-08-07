16ca6889e85ac7f2d4bda2aa80c2f1db41b9dc3d
fayjullahh <fayjullahh24@gmail.com>
2025-08-07 21:05:19 +0600

    Adding datatype and comment codes

---
+++ 2_datatypes/datatype.py
@@ -0,0 +1,48 @@
+#common datatypes
+# 1.Integer , 2.float, 3. complex, 4. boolean , 5. string
+
+#integer
+print(5)
+print(-5)
+print(1e308) # maximum of the range of integer value in python
+print(1e309) # more than maximum value of the range of integer is defined as infinite in python
+
+#float
+print(0.6)
+print(-0.6)
+print(1.7e308) # maximum of the range of float value in python
+print(1.7e309) # more than maximum value of the range of float is defined as infinite in python
+
+#complex
+print(1+5j)
+print(1-5j)
+print(4j)
+print(-5j)
+
+#boolean
+
+print(True)
+print(False)
+print(True and False)
+print(False and True)
+print(True and True)
+print(False and False)
+print(True or True)
+print(True or False)
+print(False or True)
+print(False or False)
+print(not True)
+print(not False)
+
+#string
+
+print('Hello')
+print("World")
+print("""
+      This
+      is 
+      a 
+      multiline
+      string
+""")
+
---
+++ 3_comment/comment.py
@@ -0,0 +1,2 @@
+print("Single line comment")
+"Hello everyone. Python is easy"
